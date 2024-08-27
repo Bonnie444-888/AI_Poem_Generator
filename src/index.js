f8aed309c7f88534fc8a398181b123babfaecf81
Bonnie444-888 <isabel.kedibone@gmail.com>
2024-08-27 11:54:23 +0200

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "Give me a second while I generate a beautiful peace of poetry for you",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
